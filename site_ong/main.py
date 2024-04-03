72d3c4d488b0328bf260ad4eac4cb3f281e61da4
ivansantiagojr <ivansantiago.junior@gmail.com>
2024-04-02 21:23:59 -0300

    configuração inicial de templates

--- site_ong/main.py
+++ site_ong/main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+from site_ong.templates_conf import templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="site_ong/static"), name="static")
 
-@app.get('/')
-def read_root():
-    return {'message': 'Hello, world!'}
+
+@app.get('/', response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse(name='index.html', request=request)
