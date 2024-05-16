147c5cfc86c5e0eaad9d1170245bea8a686beb47
Ivan Santiago <65205079+ivansantiagojr@users.noreply.github.com>
2024-05-16 20:37:54 -0300

    Correcoes gerais (#11)

--- site_ong/users/routes.py
+++ site_ong/users/routes.py
@@ -1,8 +1,8 @@
-from http.client import HTTPException
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from site_ong.auth.routes import CurrentUser
@@ -18,11 +18,8 @@ Session = Annotated[Session, Depends(get_session)]
 
 @router.get('/', response_class=HTMLResponse)
 def users(request: Request, user: CurrentUser, session: Session):
-    if not user:
+    if not user or user.role != 'admin':
         return RedirectResponse('/login')
-
-    if user.role != 'admin':
-        raise HTTPException(status_code=403, detail='Permissão negada')
 
     users = session.query(User).all()
     return templates.TemplateResponse(
@@ -32,14 +29,14 @@ def users(request: Request, user: CurrentUser, session: Session):
 
 @router.get('/novo', response_class=HTMLResponse)
 def new_user_page(request: Request, user: CurrentUser):
-    if not user:
+    if not user or user.role != 'admin':
         return RedirectResponse('/login')
     return templates.TemplateResponse('new_user.html', {'request': request})
 
 
 @router.post('/novo', response_class=HTMLResponse)
 async def new_user(request: Request, user: CurrentUser, session: Session):
-    if not user:
+    if not user or user.role != 'admin':
         return RedirectResponse('/login')
 
     form = await request.form()
@@ -63,47 +60,76 @@ async def new_user(request: Request, user: CurrentUser, session: Session):
 
 @router.get('/deletar', response_class=HTMLResponse)
 def delete_user_page(request: Request, user: CurrentUser):
-    if not user:
+    if not user or user.role != 'admin':
         return RedirectResponse('/login')
     return templates.TemplateResponse('delete_user.html', {'request': request})
 
 
 @router.post('/deletar')
 async def delete_user(request: Request, user: CurrentUser, session: Session):
-    if not user:
+    if not user or user.role != 'admin':
         return RedirectResponse('/login')
 
     form = await request.form()
     username = form.get('username')
+
+    if username == user.username:
+        return templates.TemplateResponse(
+            'delete_user.html',
+            {'request': request, 'error': 'Você não pode deletar a si mesmo.'},
+        )
 
     user = session.query(User).filter(User.username == username).first()
     if user:
         session.delete(user)
         session.commit()
+    else:
+        return templates.TemplateResponse(
+            'delete_user.html',
+            {'request': request, 'error': 'Usuário não encontrado.'},
+        )
 
     return RedirectResponse('/dashboard', status_code=303)
 
 
-@router.get('/editar', response_class=HTMLResponse)
-def edit_user_page(request: Request, user: CurrentUser):
-    if not user:
+@router.get('/editar/{user_id}', response_class=HTMLResponse)
+def edit_user_page(
+    user_id: int, request: Request, user: CurrentUser, session: Session
+):
+    if not user or user.role != 'admin':
         return RedirectResponse('/login')
-    return templates.TemplateResponse('edit_user.html', {'request': request})
+
+    user = session.scalar(select(User).where(User.id == user_id))
+    return templates.TemplateResponse(
+        'edit_user.html', {'request': request, 'user': user}
+    )
 
 
-@router.post('/editar')
-async def edit_user(request: Request, user: CurrentUser, session: Session):
-    if not user:
+@router.post('/editar/{user_id}')
+async def edit_user(
+    user_id: int, request: Request, user: CurrentUser, session: Session
+):
+    if not user or user.role != 'admin':
         return RedirectResponse('/login')
 
     form = await request.form()
-    username = form.get('username')
-    new_username = form.get('new_username')
-    new_password = form.get('new_password')
-    user = session.query(User).filter(User.username == username).first()
+    full_name = form.get('name')
+    password = form.get('password')
+
+    user = session.scalar(select(User).where(User.id == user_id))
     if user:
-        user.username = new_username
-        user.password = get_password_hash(new_password)
+        user.full_name = full_name
+        user.role = form.get('role')
+
+        if password:
+            user.password = get_password_hash(password)
+
         session.commit()
+        session.refresh(user)
+    else:
+        return templates.TemplateResponse(
+            'edit_user.html',
+            {'request': request, 'error': 'Usuário não encontrado.'},
+        )
 
     return RedirectResponse('/dashboard', status_code=303)
