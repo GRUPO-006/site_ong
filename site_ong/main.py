cdd22ff324c6435bc04275c91a16b09777aeccdc
Ivan Santiago <65205079+ivansantiagojr@users.noreply.github.com>
2024-05-16 23:15:30 -0300

    corrigindo a cli de admin (#13)

--- site_ong/main.py
+++ site_ong/main.py
@@ -3,42 +3,18 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
-from sqlalchemy import select
-from sqlalchemy.orm import sessionmaker
+
 
 from site_ong.auth.routes import router as auth_router
 from site_ong.dashboard.routes import router as dashboard_router
-from site_ong.database import engine
+
 from site_ong.posts.routes import router as posts_router
-from site_ong.security import get_password_hash
+
 from site_ong.templates_conf import templates
-from site_ong.users.models import User
+
 from site_ong.users.routes import router as users_router
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    session = SessionLocal()
-    db_user = session.scalar(select(User).where(User.username == 'admin'))
-    if db_user:
-        session.delete(db_user)
-        session.commit()
-
-    hashed_password = get_password_hash('admin')
-    db_user = User(
-        full_name='Admin',
-        username='admin',
-        password=hashed_password,
-        role='admin',
-    )
-    session.add(db_user)
-    session.commit()
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.mount('/static', StaticFiles(directory='site_ong/static'), name='static')
 
